44eb2a81b335552526a60874abd98c4adba28104
Ryan Hoover <ryan.hoover@wpengine.com>
2017-06-29 16:04:02 -0500

    add numLines option

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const csv = require( 'node-csv' ).createParser()
 const request = require( 'request' )
 const read = require( 'readline-sync' )
 
-var csvFile = read.question( 'What is the CSV file with the domains? ' );
-
+var csvFile = read.question( 'What is the path to the CSV file with the domains? ' );
+var lineCount = read.questionInt( 'How many lines should a minified file have at maximum? (default: 3) ', {
+    defaultInput: 3
+} )
+console.log( lineCount )
 csv.mapFile( csvFile, function ( err, sites ) {
     _.each( sites, ( site ) => {
         if ( site.domain ) {
